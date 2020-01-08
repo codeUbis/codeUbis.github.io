917e48c65cf3996f295d673d721fec2b5a9dfa51
Dixy <dixy@Ubis.local>
2020-01-08 19:15:51 +0200

    Responsive update

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -1,5 +1,21 @@
 
-var mySwiper = new Swiper ('.swiper-container', {
+let navBar = document.getElementById('menu');
+let navBarToggle = document.getElementById('navbar-toggle');
+let contactsForm = document.getElementById("form");
+
+// Burger menu;
+navBarToggle.addEventListener('click', function () {
+  navBar.classList.toggle('active');
+});
+// Scroll class remove;
+window.addEventListener('scroll', function (){
+  let scrollPosition = window.scrollY;
+  if(scrollPosition >= 10){
+    navBar.classList.remove('active');
+    console.log(scrollPosition);
+  }
+});
+let mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -20,3 +36,17 @@ var mySwiper = new Swiper ('.swiper-container', {
   $('[data-fancybox="gallery"]').fancybox({
     // Options will go here
   });
+
+// // map functions
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+// }
+
+// Contacts
+function contact(){
+  contactsForm.classList.toggle("formDisplay");
+}
